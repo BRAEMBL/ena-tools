e4f102cb82818deacc7ee95d9ea7977c8c84284b
Michael Nuhn <m.nuhn@braembl.org.au>
2014-10-20 09:22:08 +1000

    Not helpful in public release

--- project_lib/BRAEMBL/ENA/Submission/Rest/ST131/DomainObjectBuilder/Director.pm
+++
@@ -1,6 +0,0 @@
-package BRAEMBL::ENA::Submission::Rest::DomainObjectBuilder::ST131::Director;
-
-use Moose;
-extends 'BRAEMBL::ENA::Submission::Rest::DomainObjectBuilder::BacterialStrains::Director';
-
-1;
